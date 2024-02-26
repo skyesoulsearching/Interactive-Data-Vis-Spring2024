d96eac63f2e412887a67545bbe5f33e51edea340
Marilyn Moy <ms.marilynmoy@gmail.com>
2024-02-26 17:04:15 -0500

    put in the click counter and a human alert

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,28 @@
+
+
 console.log('hello world');
+
+
+const clickButton = document.getElementById('clickButton');
+const clickCount = document.getElementById('clickCount');
+let count = 0;
+
+clickButton.onclick = function() {
+  count++;
+  clickCount.innerText = count;
+};
+
+
+const submitInfoButton = document.getElementById('submitInfoButton');
+const nameInput = document.getElementById('nameInput');
+const ageInput = document.getElementById('ageInput');
+
+submitInfoButton.onclick = function() {
+  const name = nameInput.value;
+  const age = ageInput.value;
+
+  const userInfo = { name, age };
+  console.log(userInfo);
+
+  alert(`People say age is just a number, buttttt I think it's awesome how, ${name}, you have lived on this planet for ${age} years! MUCH LOVE FELLOW HUMAN BEING~`);
+};
